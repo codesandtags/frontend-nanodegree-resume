6a1ad887cc1377578619aa542d5fe472c35d8d7d
Edwin Torres <edwin.torres@prodigious.com>
2016-07-18 16:41:02 -0500

    Adding .gitignore files and super basic bio JSON

--- js/resumeBuilder.js
+++ js/resumeBuilder.js
@@ -1,3 +1,19 @@
 /*
-This is empty on purpose! Your code to build the resume will go here.
+ This is empty on purpose! Your code to build the resume will go here.
  */
+$('#main').append('Hola k hace bebe');
+
+var bio = {
+        "name": "cocoman",
+        "role": "webdeveloper",
+        "contacts": {
+            "mobile": "312..",
+            "facebook": "facebook"
+        },
+        "welcomeMEssage": "habia una vez una iguabna",
+        "skills": [],
+        "bioPic": "htpp://url"
+    },
+    projects = {},
+    education = {},
+    work = {};
